ec39d9c06066810e27cc4b106c2dad4a15f692dc
Mike Mansell <me@michaelmansell.com>
2017-06-16 23:45:34 -0700

    Refactored meta data parsing
    This adds support for the inline metadata (ie. _dqconfig_meta_XXX)
    It also enables metadata to be defined in one config source and consumed
    in another.

--- common-config-core/src/main/java/com/diamondq/common/config/format/AbstractStdConfigParser.java
+++ common-config-core/src/main/java/com/diamondq/common/config/format/AbstractStdConfigParser.java
@@ -5,9 +5,11 @@ import com.diamondq.common.config.spi.ConfigParser;
 import com.diamondq.common.config.spi.ConfigProp;
 import com.diamondq.common.config.spi.NodeType;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.SortedMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -28,6 +30,49 @@ public abstract class AbstractStdConfigParser implements ConfigParser {
 
 	private static final AtomicInteger					sPrefixCount			= new AtomicInteger(0);
 
+	private static class MetaInfo {
+		Optional<ConfigProp>	type		= Optional.empty();
+
+		Optional<ConfigProp>	factory		= Optional.empty();
+
+		Optional<ConfigProp>	factoryArg	= Optional.empty();
+
+		Map<String, ConfigProp>	meta		= new HashMap<>();
+	}
+
+	private MetaInfo resolveMeta(Object metaObj, String pSourceName) {
+		if (metaObj instanceof Map) {
+			@SuppressWarnings("unchecked")
+			Map<String, Object> metaMap = (Map<String, Object>) metaObj;
+
+			/* Store all the meta data into the meta portion of the node builder */
+
+			MetaInfo result = new MetaInfo();
+			for (Map.Entry<String, Object> metaPair : metaMap.entrySet()) {
+				Object metaValueObj = metaPair.getValue();
+				if ((metaValueObj instanceof Map) || (metaValueObj instanceof List))
+					throw new IllegalStateException();
+				String metaKey = metaPair.getKey();
+				if (sTYPE_FACTORY_KEY.equals(metaKey))
+					result.factory = Optional
+						.of(ConfigProp.builder().configSource(pSourceName).value(metaValueObj.toString()).build());
+				else if (sTYPE_FACTORY_ARG_KEY.equals(metaKey))
+					result.factoryArg = Optional
+						.of(ConfigProp.builder().configSource(pSourceName).value(metaValueObj.toString()).build());
+				else if (sTYPE_TYPE_KEY.equals(metaKey))
+					result.type = Optional
+						.of(ConfigProp.builder().configSource(pSourceName).value(metaValueObj.toString()).build());
+				else
+					result.meta.put(metaKey,
+						ConfigProp.builder().configSource(pSourceName).value(metaValueObj.toString()).build());
+			}
+
+			return result;
+		}
+		else
+			throw new IllegalStateException();
+	}
+
 	protected ConfigNode map(String pSourceName, String pName, Object o) {
 		String prefix = sPrefixMap.get(pSourceName);
 		if (prefix == null) {
@@ -39,9 +84,9 @@ public abstract class AbstractStdConfigParser implements ConfigParser {
 		if (o instanceof Map) {
 			@SuppressWarnings("unchecked")
 			Map<String, Object> map = (Map<String, Object>) o;
-			Optional<ConfigProp> type = Optional.empty();
-			Optional<ConfigProp> factory = Optional.empty();
-			Optional<ConfigProp> factoryArg = Optional.empty();
+			MetaInfo parentMeta = new MetaInfo();
+			Map<String, MetaInfo> pendingMeta = new HashMap<>();
+			Map<String, ConfigNode> existingNodes = new HashMap<>();
 			for (Map.Entry<String, Object> pair : map.entrySet()) {
 
 				String key = pair.getKey();
@@ -50,39 +95,55 @@ public abstract class AbstractStdConfigParser implements ConfigParser {
 
 				if (sMETA_KEY.equals(key)) {
 					Object metaObj = pair.getValue();
-					if (metaObj instanceof Map) {
-						@SuppressWarnings("unchecked")
-						Map<String, Object> metaMap = (Map<String, Object>) metaObj;
-
-						/* Store all the meta data into the meta portion of the node builder */
-
-						for (Map.Entry<String, Object> metaPair : metaMap.entrySet()) {
-							Object metaValueObj = metaPair.getValue();
-							if ((metaValueObj instanceof Map) || (metaValueObj instanceof List))
-								throw new IllegalStateException();
-							String metaKey = metaPair.getKey();
-							if (sTYPE_FACTORY_KEY.equals(metaKey))
-								factory = Optional.of(ConfigProp.builder().configSource(pSourceName)
-									.value(metaValueObj.toString()).build());
-							else if (sTYPE_FACTORY_ARG_KEY.equals(metaKey))
-								factoryArg = Optional.of(ConfigProp.builder().configSource(pSourceName)
-									.value(metaValueObj.toString()).build());
-							else if (sTYPE_TYPE_KEY.equals(metaKey))
-								type = Optional.of(ConfigProp.builder().configSource(pSourceName)
-									.value(metaValueObj.toString()).build());
-							else
-								builder.putMetaData(metaKey, ConfigProp.builder().configSource(pSourceName)
-									.value(metaValueObj.toString()).build());
-						}
+					parentMeta = resolveMeta(metaObj, pSourceName);
+				}
+				else if (key.startsWith(sMETA_KEY)) {
+					Object metaObj = pair.getValue();
+					key = key.substring(sMETA_KEY.length());
+					MetaInfo meta = resolveMeta(metaObj, pSourceName);
+					ConfigNode childNode = existingNodes.get(key);
+					if (childNode != null) {
+						childNode = mergeMeta(meta, childNode);
+						existingNodes.put(key, childNode);
+						builder.putChildren(key, childNode);
 					}
 					else
-						throw new IllegalStateException();
+						pendingMeta.put(key, meta);
 				}
-				else
-					builder.putChildren(pair.getKey(), map(pSourceName, pair.getKey(), pair.getValue()));
+				else {
+					ConfigNode childNode = map(pSourceName, key, pair.getValue());
+					MetaInfo meta = pendingMeta.remove(key);
+					if (meta != null) {
+						childNode = childNode.withType(NodeType.builder().isExplicitType(meta.type.isPresent())
+							.type(meta.type).factory(meta.factory).factoryArg(meta.factoryArg).build());
+					}
+					existingNodes.put(key, childNode);
+					builder.putChildren(key, childNode);
+				}
 			}
-			builder = builder.type(NodeType.builder().isExplicitType(type.isPresent()).type(type).factory(factory)
-				.factoryArg(factoryArg).build());
+			if (pendingMeta.isEmpty() == false) {
+
+				/*
+				 * For each of the pending meta data that never found a matching entry, create an 'empty' property and
+				 * hopefully it'll merge into the 'real' one in a later config source
+				 */
+
+				for (Map.Entry<String, MetaInfo> pair : pendingMeta.entrySet()) {
+
+					ConfigNode.Builder emptyBuilder = ConfigNode.builder().name(pair.getKey());
+					emptyBuilder = emptyBuilder.type(NodeType.builder().isExplicitType(false)
+						.type(ConfigProp.builder().configSource(pSourceName).value(String.class.getName()).build())
+						.build());
+
+					emptyBuilder = emptyBuilder.value(ConfigProp.builder().configSource(pSourceName).value(o.toString()).build());
+					ConfigNode emptyNode = emptyBuilder.build();
+					MetaInfo emptyMeta = pair.getValue();
+					emptyNode = mergeMeta(emptyMeta, emptyNode);
+					builder.putChildren(pair.getKey(), emptyNode);
+				}
+			}
+			builder = builder.type(NodeType.builder().isExplicitType(parentMeta.type.isPresent()).type(parentMeta.type)
+				.factory(parentMeta.factory).factoryArg(parentMeta.factoryArg).build());
 
 		}
 		else if (o instanceof List) {
@@ -159,6 +220,25 @@ public abstract class AbstractStdConfigParser implements ConfigParser {
 
 		}
 		return builder.build();
+	}
+
+	private ConfigNode mergeMeta(MetaInfo pMeta, ConfigNode pChildNode) {
+		if ((pMeta.type.isPresent()) || (pMeta.factory.isPresent()) || (pMeta.factoryArg.isPresent()))
+			pChildNode = pChildNode.withType(NodeType.builder().isExplicitType(pMeta.type.isPresent()).type(pMeta.type)
+				.factory(pMeta.factory).factoryArg(pMeta.factoryArg).build());
+		if (pMeta.meta.isEmpty() == false) {
+			SortedMap<String, ConfigProp> existingMeta = pChildNode.getMetaData();
+			Map<String, ConfigProp> newMeta;
+			if (existingMeta == null)
+				newMeta = pMeta.meta;
+			else {
+				newMeta = new HashMap<>();
+				newMeta.putAll(existingMeta);
+				newMeta.putAll(pMeta.meta);
+			}
+			pChildNode = pChildNode.withMetaData(newMeta);
+		}
+		return pChildNode;
 	}
 
 }
